109ac5d03024ef3278266ec7ae0ab6931b710f08
dariakyrychenko <dkyrychenko@binariks.com>
2025-07-14 13:52:34 +0300

    setup tree shaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   plugins: [
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
+      analyzerMode: isProd ? 'static' : 'disabled',
       reportFilename: 'bundle-report.html',
       openAnalyzer: false,
     }),
   ],
+  mode: isProd ? 'production' : 'development',
+  optimization: {
+    usedExports: isProd, // Enable tree shaking only in production
+  }
 };
