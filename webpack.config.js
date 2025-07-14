5d72f47b25cf6ba8f9989cf4a001ad38355c607d
dariakyrychenko <dkyrychenko@binariks.com>
2025-07-14 15:06:53 +0300

    add .env

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
     new BundleAnalyzerPlugin({
-      analyzerMode: isProd ? 'static' : 'disabled',
-      reportFilename: 'bundle-report.html',
-      openAnalyzer: false,
+      analyzerMode: isProd ? process.env.ANALYZER_MODE_PROD : process.env.ANALYZER_MODE_DEV,
+      reportFilename: isProd ? process.env.REPORT_FILENAME_PROD : process.env.REPORT_FILENAME_DEV,
+      openAnalyzer: isProd ? process.env.OPEN_ANALYZER_PROD === 'true' : process.env.OPEN_ANALYZER_DEV === 'true',
     }),
   ],
   mode: isProd ? 'production' : 'development',
